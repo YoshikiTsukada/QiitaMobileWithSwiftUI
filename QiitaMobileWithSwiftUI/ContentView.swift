918ca7e98a093df10e5e94cb463e1de24a6c0497
YoshikiTsukada <tsukadayyy@gmail.com>
2020-05-07 20:34:31 +0900

    First commit

---
+++ QiitaMobileWithSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QiitaMobileWithSwiftUI
+//
+//  Created by Yoshiki Tsukada on 2020/05/07.
+//  Copyright © 2020 Yoshiki Tsukada. All rights reserved.
+//
+
+import SwiftUI
+
+//struct ContentView: View {
+//    init() {
+//    }
+//
+//    var body: some View {
+//        Text("Hello, World!")
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
